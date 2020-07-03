2a36b4e4e3f5d0def5382b3cfc572a01a02238cb
Zachary Weeden <zdw7287@rit.edu>
2020-07-02 20:48:23 -0400

    robots.txt

--- zweedendev/urls.py
+++ zweedendev/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
-urlpatterns = [path("", views.index, name="index"),] + static(
-    settings.STATIC_URL, document_root=settings.STATIC_ROOT
-)
+urlpatterns = [
+    path("", views.index, name="index"),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
